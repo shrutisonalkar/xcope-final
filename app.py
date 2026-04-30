884957d8ad59d440067da84fa41ec2cd1722ec36
Shruti <shrutisonalkar@email.com>
2026-05-01 03:02:53 +0530

    added eventlet monkey patch

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, jsonify, request, send_file
 from flask_socketio import SocketIO, emit
 from textblob import TextBlob
